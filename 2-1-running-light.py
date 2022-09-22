f57c8f877223a7e02a4c502ce3109578967c2f6f
Natozzo-san <71597186+Natozzo-san@users.noreply.github.com>
2022-09-22 15:29:06 +0300

    Update 2-1-running-light.py

--- 2-1-running-light.py
+++ 2-1-running-light.py
@@ -10,3 +10,28 @@ for i in range(3):
             time.sleep(0.2)
             GPIO.output(led, 0)
 GPIO.output(leds, 0)
+
+
+
+
+
+
+
+def dec2bin(value): 
+    return [int(bit) for bit in format(value).zfill(8)]
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+GPIO.setup(dac, GPIO.OUT)
+try:
+    t = float(input())
+    while True:
+        for i in range(256):
+             GPIO.output(dac, i)
+             time.sleep(t/(256*4))
+
+finally:    
+    GPIO.output(dac, 0)
+    GPIO.cleanup()
